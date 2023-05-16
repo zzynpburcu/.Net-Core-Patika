b42f5c7d19d49d598a6de259fe205470cfaee0cf
zeynep <44863956+zzynpburcu@users.noreply.github.com>
2023-05-17 00:23:55 +0300

    Add files via upload

---
+++ Switch-Case/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Switch_Case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+
+            //Expression
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("Ocak Ayındasınız");
+                    break;
+                case 2:
+                    Console.WriteLine("Şubat Ayındasınız");
+                    break;    
+                case 3:
+                    Console.WriteLine("Mart Ayındasınız");
+                    break;
+                  case 5:
+                    Console.WriteLine("Mayıs Ayındasınız");
+                    break;    
+                
+                default:
+                break;
+            }
+
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                        Console.WriteLine("Kış Mevsimindesiniz");
+                        break;
+
+                case 3:
+                case 4:
+                case 5:
+                         Console.WriteLine("İlkbahar Mevsimindesiniz");
+                         break;
+                
+                default:
+                break;
+            }
+        }
+    }
+}
