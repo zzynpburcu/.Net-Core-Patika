01318c2f2781024b6798948e622ac715ec7c0f60
zeynep <44863956+zzynpburcu@users.noreply.github.com>
2023-05-25 00:14:46 +0300

    Add files via upload

---
+++ Patika-CSharp-HW2/Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Koleksiyonlar_Soru_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Soru 3
+            //Klavyeden girilen cümle içerisindeki sesli harfleri bir dizi içerisinde saklayan ve dizinin elemanlarını sıralayan programı yazınız.
+
+
+            string cumle = Console.ReadLine();
+            char[] cumleDizi = cumle.ToCharArray();
+            char[] sesliHarfler = { 'a', 'e', 'ı', 'i', 'o', 'ö', 'u', 'ü', 'A', 'E', 'I', 'İ', 'O', 'Ö', 'U', 'Ü' };
+            string cumleSesliHarflerString = "";
+            foreach (var item in cumleDizi)
+            {
+                if (Array.IndexOf(sesliHarfler, item) >= 0)
+                    cumleSesliHarflerString += item;
+            }
+            char[] cumleSesliHarfler = cumleSesliHarflerString.ToCharArray();
+            foreach (var item in cumleSesliHarfler)
+                Console.WriteLine(item);
+
+        }
+    }
+}
