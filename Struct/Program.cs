0c99055dcbd40527b04b38a647fef0231a08b827
zeynep <44863956+zzynpburcu@users.noreply.github.com>
2023-05-29 22:46:22 +0300

    Add files via upload

---
+++ Struct/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dikdörtgen dikdörtgen = new Dikdörtgen();
+            dikdörtgen.KısaKenar = 3;
+            dikdörtgen.UzunKenar = 4;
+            Console.WriteLine("Class Alan Hesabı: {0}", dikdörtgen.AlanHesapla());
+
+            // struch da new ile tanımlamaya gerek yok bu şekilde de tanımlayabiliriz   
+            Dikdörtgen_Struct dikdörtgen_struct = new Dikdörtgen_Struct(3, 4);
+            /*   dikdörtgen_struct.KısaKenar = 3;
+              dikdörtgen_struct.UzunKenar = 4; */
+            Console.WriteLine("Struct Alan Hesabı: {0}", dikdörtgen_struct.AlanHesapla());
+        }
+    }
+    class Dikdörtgen
+    {
+        public int KısaKenar;
+        public int UzunKenar;
+        public long AlanHesapla()
+        {
+            return this.KısaKenar * this.UzunKenar;
+        }
+    }
+    struct Dikdörtgen_Struct
+    {
+        public int KısaKenar;
+        public int UzunKenar;
+        //Buş constructor tanımlayamıyoruz
+        public Dikdörtgen_Struct(int kisakenar, int uzunkenar)
+        {
+            this.KısaKenar = kisakenar;
+            this.UzunKenar = uzunkenar;
+        }
+        public long AlanHesapla()
+        {
+            return this.KısaKenar * this.UzunKenar;
+        }
+    }
+}
