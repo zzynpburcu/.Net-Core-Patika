828da8d916bf475e53ad78afd870bc70433a2de1
zeynep <zeynepkayhan7@gmail.com>
2023-06-04 03:39:25 +0300

    added todo app

---
+++ ToDo-Console-App/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ToDo_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> kisiler = new Dictionary<int, string>();
+
+            Takım person = new Takım(18, "Zeynep Burcu");
+            person.AddPerson(kisiler);
+        }
+    }
+}
